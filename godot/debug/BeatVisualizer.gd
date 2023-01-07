3734f4459da5bfd23cab5da04089987164db005b
Paul Norberger <panor@fastmail.com>
2023-01-07 16:46:10 +0100

    More work on conductor and input handling

--- godot/debug/BeatVisualizer.gd
+++ godot/debug/BeatVisualizer.gd
@@ -4,16 +4,8 @@ onready var beat_hit_zone_label = get_node("%BeatHitZoneLabel")
 onready var conductor = get_node("%Conductor")
 onready var beat_indicator : TextureRect = get_node("%BeatIndicator")
 
-func _process(delta):
-	match conductor.current_beat_zone:
-		0:
-			beat_hit_zone_label.text = "MISS"
-		1:
-			beat_hit_zone_label.text = "TOO_EARLY"
-		2:
-			beat_hit_zone_label.text = "HIT"
-		3:
-			beat_hit_zone_label.text = "TOO_LATE"
+#func _process(delta):
+#	beat_hit_zone_label.text = conductor.get_beat_hit_zone_string(conductor.current_beat_zone)
 
 
 func _on_Conductor_enter_beat_close_zone():
@@ -29,3 +21,6 @@ func _on_Conductor_exit_beat_hit_zone():
 func _on_Conductor_exit_beat_close_zone():
 	pass
 	# beat_indicator.rect_scale = Vector2(0.4, 0.4)
+
+func _on_Conductor_hit_result(hit_zone):
+	beat_hit_zone_label.text = conductor.get_beat_hit_zone_string(hit_zone)
--- godot/game/conductor/Conductor.gd
+++ godot/game/conductor/Conductor.gd
@@ -4,11 +4,11 @@ onready var music_player : AudioStreamPlayer = get_node("MusicPlayer")
 
 const BPM := 128
 const SECONDS_PER_BEAT : float = 60.0 / BPM
-const TIME_UNTIL_FIRST_BEAT := 0
-# const TIME_UNTIL_FIRST_BEAT := 0.034
+# const TIME_UNTIL_FIRST_BEAT := 0
+const TIME_UNTIL_FIRST_BEAT := 0.0
 
-const SECONDS_TOLERANCE_HIT : float = (SECONDS_PER_BEAT / 5) * 1
-const SECONDS_TOLERANCE_CLOSE : float = SECONDS_TOLERANCE_HIT * 2
+const SECONDS_TOLERANCE_HIT : float = 0.06
+const SECONDS_TOLERANCE_CLOSE : float = SECONDS_TOLERANCE_HIT + 0.08
 
 enum BEAT_HIT_ZONE {
 	MISS
@@ -16,6 +16,17 @@ enum BEAT_HIT_ZONE {
 	HIT,
 	TOO_LATE
 }
+
+func get_beat_hit_zone_string(hit_zone):
+	match hit_zone:
+		0:
+			return "MISS"
+		1:
+			return "TOO_EARLY"
+		2:
+			return "HIT"
+		3:
+			return "TOO_LATE"
 
 func _ready():
 	music_player.play()
@@ -29,6 +40,8 @@ signal enter_beat_hit_zone
 signal exit_beat_hit_zone
 signal enter_beat_close_zone
 signal exit_beat_close_zone
+
+signal hit_result(hit_zone)
 
 func _physics_process(delta):
 	var track_position_seconds = music_player.get_playback_position()
@@ -45,19 +58,23 @@ func _physics_process(delta):
 	
 	if abs(seconds_to_closest_beat) < SECONDS_TOLERANCE_HIT:
 		if current_beat_zone != BEAT_HIT_ZONE.HIT:
-			emit_signal("enter_beat_hit_zone")
 			current_beat_zone = BEAT_HIT_ZONE.HIT
+			emit_signal("enter_beat_hit_zone")
 	elif abs(seconds_to_closest_beat) < SECONDS_TOLERANCE_CLOSE:
-		if current_beat_zone == BEAT_HIT_ZONE.MISS:
-			emit_signal("enter_beat_close_zone")
-			current_beat_zone = BEAT_HIT_ZONE.TOO_EARLY
-		if current_beat_zone == BEAT_HIT_ZONE.HIT:
-			emit_signal("exit_beat_hit_zone")
+		if seconds_to_closest_beat < 0 and current_beat_zone != BEAT_HIT_ZONE.TOO_LATE:
 			current_beat_zone = BEAT_HIT_ZONE.TOO_LATE
+			emit_signal("exit_beat_hit_zone")
+		elif seconds_to_closest_beat >= 0 and current_beat_zone != BEAT_HIT_ZONE.TOO_EARLY:
+			current_beat_zone = BEAT_HIT_ZONE.TOO_EARLY
+			emit_signal("enter_beat_close_zone")
 	elif current_beat_zone == BEAT_HIT_ZONE.TOO_LATE:
-		emit_signal("exit_beat_close_zone")
 		current_beat_zone = BEAT_HIT_ZONE.MISS
+		emit_signal("exit_beat_close_zone")
 	
 	if beat_position > last_beat_pos:
 		# print(beat_position)
 		last_beat_pos = beat_position
+
+func _input(event):
+	if event.is_action_pressed("beat_hit"):
+		emit_signal("hit_result", current_beat_zone)
