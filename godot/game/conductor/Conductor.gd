dca9beb129154dc79c7c9bf8818e7196f1dc8810
Paul Norberger <panor@fastmail.com>
2023-01-08 18:08:41 +0100

    Made some changes to conductor and added song

--- godot/game/conductor/Conductor.gd
+++ godot/game/conductor/Conductor.gd
@@ -2,13 +2,15 @@ extends Node
 
 onready var music_player : AudioStreamPlayer = get_node("MusicPlayer")
 
-const BPM := 128
-const SECONDS_PER_BEAT : float = 60.0 / BPM
-# const TIME_UNTIL_FIRST_BEAT := 0
-const TIME_UNTIL_FIRST_BEAT := 0.0
+export(int) var bpm := 128
+var seconds_per_beat : float = 60.0 / bpm
 
-const SECONDS_TOLERANCE_HIT : float = 0.06
-const SECONDS_TOLERANCE_CLOSE : float = SECONDS_TOLERANCE_HIT + 0.08
+export(float, 0, 5) var time_until_first_beat := 0.0
+
+export(float, 0, 1) var seconds_tolerance_hit : float = 0.14
+export(float, 0, 1) var seconds_tolerance_close : float = seconds_tolerance_hit + 0.08
+
+var hit_pattern : String = "0100010001000100010001000100010001010010001010010100101001000101001010010001000100010001000100010001000101001000101001010010100100010100101001010010001010010100101001000101001010000"
 
 enum BEAT_HIT_ZONE {
 	MISS
@@ -30,8 +32,8 @@ func get_beat_hit_zone_string(hit_zone):
 
 func _ready():
 	music_player.play()
-	print("hit: " + str(SECONDS_TOLERANCE_HIT))
-	print("close: " + str(SECONDS_TOLERANCE_CLOSE))
+	print("hit: " + str(seconds_tolerance_hit))
+	print("close: " + str(seconds_tolerance_close))
 
 var last_beat_pos := -1
 var current_beat_zone : int = BEAT_HIT_ZONE.MISS
@@ -48,20 +50,20 @@ func _physics_process(delta):
 	var track_position_seconds = music_player.get_playback_position()
 	track_position_seconds += AudioServer.get_time_since_last_mix()
 	track_position_seconds -= AudioServer.get_output_latency()
-	track_position_seconds -= TIME_UNTIL_FIRST_BEAT
+	track_position_seconds -= time_until_first_beat
 	
-	var beat_position = int(track_position_seconds / SECONDS_PER_BEAT)
+	var beat_position = int(track_position_seconds / seconds_per_beat)
 	
-	closest_beat_position = round(track_position_seconds / SECONDS_PER_BEAT)
-	var closest_beat_seconds = TIME_UNTIL_FIRST_BEAT + (closest_beat_position * SECONDS_PER_BEAT)
+	closest_beat_position = round(track_position_seconds / seconds_per_beat)
+	var closest_beat_seconds = time_until_first_beat + (closest_beat_position * seconds_per_beat)
 	
 	var seconds_to_closest_beat = closest_beat_seconds - track_position_seconds
 	
-	if abs(seconds_to_closest_beat) < SECONDS_TOLERANCE_HIT:
+	if abs(seconds_to_closest_beat) < seconds_tolerance_hit:
 		if current_beat_zone != BEAT_HIT_ZONE.HIT:
 			current_beat_zone = BEAT_HIT_ZONE.HIT
 			emit_signal("enter_beat_hit_zone")
-	elif abs(seconds_to_closest_beat) < SECONDS_TOLERANCE_CLOSE:
+	elif abs(seconds_to_closest_beat) < seconds_tolerance_close:
 		if seconds_to_closest_beat < 0 and current_beat_zone != BEAT_HIT_ZONE.TOO_LATE:
 			current_beat_zone = BEAT_HIT_ZONE.TOO_LATE
 			emit_signal("exit_beat_hit_zone")
